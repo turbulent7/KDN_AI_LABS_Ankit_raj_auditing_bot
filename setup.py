0a1b37fa4db1e0012c01b89681e6f9f5bb4f26d5
turbulent7 <arvkj04943@gmail.com>
2025-02-12 15:41:15 +0530

    updated setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ankit Raj',
+    author_email= 'arvkj04943@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
