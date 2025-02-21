962aa57f800a99dbfbae6fa1a0054c56910f2639
turbulent7 <arvkj04943@gmail.com>
2025-02-21 16:18:35 +0530

    updated prompts file

--- src/prompt.py
+++ src/prompt.py
@@ -3,7 +3,7 @@ system_prompt = (
     "You are an assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
-    "don't know but you can learn and become better. Use six sentences maximum and keep the "
+    "don't know but you can learn and become better. Use 5 sentences maximum and keep the "
     "answer concise."
     "\n\n"
     "{context}"
